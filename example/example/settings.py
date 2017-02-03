ce3fd89c8061205d76e0128340dbeb07c5d6774f
Atul Varma <varmaa@gmail.com>
2017-02-03 01:12:28 -0500

    set ALLOWED_HOSTS in example settings.py.

--- example/example/settings.py
+++ example/example/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'This is just an example app'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
