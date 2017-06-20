6cd5540fd2ad7557021d26f552ffe5d52fe2fe12
Atul Varma <varmaa@gmail.com>
2017-06-20 10:53:31 -0400

    Skip test if example app is unavailable.

--- uaa_client/tests/test_example_app.py
+++ uaa_client/tests/test_example_app.py
@@ -1,7 +1,7 @@
 import sys
 import subprocess
 from pathlib import Path
-from unittest import TestCase
+from unittest import TestCase, SkipTest
 
 
 ROOT_DIR = Path(__file__).resolve().parent.parent.parent
@@ -13,6 +13,11 @@ class ExampleAppTests(TestCase):
         # This is partly a regression test for the following issue:
         #
         # https://github.com/18F/cg-django-uaa/issues/18
+
+        if not EXAMPLE_APP_DIR.exists():
+            # We're being run from an installed package or something, just
+            # skip this test.
+            raise SkipTest('Example app dir unavailable to distribution')
 
         subprocess.check_output(
             [sys.executable, 'manage.py', 'test'],
