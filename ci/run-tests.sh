446edcd208ea9410e17b48e3bfa3ebd525de5627
Mark Boyd <mark.boyd@gsa.gov>
2024-01-29 15:20:40 -0500

    update test running script

--- ci/run-tests.sh
+++ ci/run-tests.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
 python -m venv venv
-source ./venv/bin/activate
+source venv/bin/activate
 
 python -m pip install -r requirements-tests.txt
-python -m pip install -e .
+
+# install package in development mode
+# python -m pip install --editable .
 
 # run tests
 tox
