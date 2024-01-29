efc691e95f5434be45788f2d454757717a804300
Mark Boyd <mark.boyd@gsa.gov>
2024-01-29 10:52:51 -0500

    update test script to install test dependencies

--- ci/run-tests.sh
+++ ci/run-tests.sh
@@ -2,8 +2,8 @@
 
 python -m venv venv
 source ./venv/bin/activate
-python -m pip install -r requirements-dev.txt
 
+python -m pip install -r requirements-tests.txt
 python -m pip install -e .
 
 # run tests
