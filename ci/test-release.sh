d4d356a8532783e6a3068d53a2a48d654aa6bd3f
Mark Boyd <mark.boyd@gsa.gov>
2024-01-29 17:20:18 -0500

    Fix testing (#72)
    * fix tox env names
    
    * update test running script
    
    * update branch
    
    * make tox run in development mode for PR tests
    
    * update prepare-release script to build release
    
    * add test release step && re-enable step to create github release
    
    * update dev docs
    
    * update 18F -> cloud-gov in repo name
    
    * update dev docs
    
    * update pipeline set-self step to use branch as source
    
    * update pipeline
    
    * update RELEASE

--- ci/test-release.sh
+++ ci/test-release.sh
@@ -3,7 +3,9 @@
 python -m venv venv
 source venv/bin/activate
 
+TAG=$(git describe --tags)
+
 python -m pip install -r requirements-tests.txt
 
-# run tests in development mode
-tox --develop
+# run tests on release
+tox --installpkg "dist/cg-django-uaa-$TAG.tar.gz"
