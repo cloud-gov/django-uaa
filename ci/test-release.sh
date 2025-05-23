49d0357aa43be2fd43073f9baa2d736d724e04e7
Mark Boyd <mark.boyd@gsa.gov>
2025-05-23 11:48:55 -0400

    fix pipeline scripts for testing and creating release

--- ci/test-release.sh
+++ ci/test-release.sh
@@ -3,9 +3,9 @@
 python -m venv venv
 source venv/bin/activate
 
-TAG=$(git describe --tags)
+TAG=$(git describe --tags | sed -e 's/^v//')
 
 python -m pip install -r requirements-tests.txt
 
 # run tests on release
-tox --installpkg "dist/cg-django-uaa-$TAG.tar.gz"
+tox --installpkg "dist/cg_django_uaa-$TAG.tar.gz"
